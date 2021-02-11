646677db4dc62ba564d967acad87e836f9d5396a
Aleksandr Kuzmenko <alex@stablex.ru>
2021-02-11 13:36:25 +0300

    update to the latest CI changes in Haxe repo

--- Gen.hx
+++ Gen.hx
@@ -18,6 +18,7 @@ typedef GhVersion = {
 class Gen {
     inline static var htmlDir = "html";
     inline static var xmlDir = "xml";
+    inline static var xmlTagsDir = "xml/refs/tags";
     inline static var themeDir = "theme";
 
     static function requestUrl(url:String):Promise<String> {
@@ -79,65 +80,69 @@ class Gen {
     ):Void {
         var dox = getDox();
         var latestVersion = getLatestVersion(versionInfo);
-        if (!exists('xml/$latestVersion')) {
-            throw 'Missing "xml/$latestVersion/"';
+        if (!exists('$xmlDir/$latestVersion') && !exists('$xmlTagsDir/$latestVersion')) {
+            throw 'Missing xmls for $latestVersion"';
         }
         deleteRecursive(htmlDir);
         createDirectory(htmlDir);
-        for (item in readDirectory(xmlDir)) {
-            var path = Path.join([xmlDir, item]);
-            if (!isDirectory(path))
-                continue;
+        function generate(dir:String) {
+            for (item in readDirectory(xmlDir)) {
+                var path = Path.join([xmlDir, item]);
+                if (!isDirectory(path))
+                    continue;
 
-            var version = item;
-            var version_long = version;
-            var versionDir, gitRef;
-            switch(versionInfo.find(function(v) return v.name == version)) {
-                case null: // it is not a release, but a branch
-                    gitRef = version;
-                    versionDir = versionedPath(gitRef);
-                    try {
-                        var info:DocInfo = Json.parse(getContent(Path.join([path, "info.json"])));
-                        version_long = '${version} @ ${info.commit.substr(0, 7)}';
-                    } catch (e:Dynamic) {}
-                case v:
-                    gitRef = v.tag_name;
-                    versionDir = versionedPath(version);
-            };
-            var outDir = Path.join([htmlDir, versionDir]);
-            createDirectory(outDir);
-            var args = [
-                "-theme", absolutePath(themeDir),
-                "--title", 'Haxe $version API',
-                "-D", "website", "https://haxe.org/",
-                "-D", "version", version_long,
-                "-D", "googleAnalytics", "UA-30943311-8",
-                "-D", "source-path", 'https://github.com/HaxeFoundation/haxe/blob/${gitRef}/std/',
-                "-i", absolutePath(path),
-                "-o", absolutePath(outDir),
-                "-ex", "microsoft",
-                "-ex", "javax",
-                "-ex", "cs.internal",
-            ];
-            if (origin != null) {
-                args = args.concat([
-                    "-D", "origin", Path.join([origin, versionDir]),
-                ]);
-            }
-            dox(args);
-
-            if (version == latestVersion) {
-                var args = args.concat([
-                    "-o", absolutePath(htmlDir),
-                ]);
+                var version = item;
+                var version_long = version;
+                var versionDir, gitRef;
+                switch(versionInfo.find(function(v) return v.name == version)) {
+                    case null: // it is not a release, but a branch
+                        gitRef = version;
+                        versionDir = versionedPath(gitRef);
+                        try {
+                            var info:DocInfo = Json.parse(getContent(Path.join([path, "info.json"])));
+                            version_long = '${version} @ ${info.commit.substr(0, 7)}';
+                        } catch (e:Dynamic) {}
+                    case v:
+                        gitRef = v.tag_name;
+                        versionDir = versionedPath(version);
+                };
+                var outDir = Path.join([htmlDir, versionDir]);
+                createDirectory(outDir);
+                var args = [
+                    "-theme", absolutePath(themeDir),
+                    "--title", 'Haxe $version API',
+                    "-D", "website", "https://haxe.org/",
+                    "-D", "version", version_long,
+                    "-D", "googleAnalytics", "UA-30943311-8",
+                    "-D", "source-path", 'https://github.com/HaxeFoundation/haxe/blob/${gitRef}/std/',
+                    "-i", absolutePath(path),
+                    "-o", absolutePath(outDir),
+                    "-ex", "microsoft",
+                    "-ex", "javax",
+                    "-ex", "cs.internal",
+                ];
                 if (origin != null) {
                     args = args.concat([
-                        "-D", "origin", origin,
+                        "-D", "origin", Path.join([origin, versionDir]),
                     ]);
                 }
                 dox(args);
+
+                if (version == latestVersion) {
+                    var args = args.concat([
+                        "-o", absolutePath(htmlDir),
+                    ]);
+                    if (origin != null) {
+                        args = args.concat([
+                            "-D", "origin", origin,
+                        ]);
+                    }
+                    dox(args);
+                }
             }
         }
+        generate(xmlDir);
+        generate(xmlTagsDir);
 
         if (cname != null)
             saveContent(Path.join([htmlDir, "CNAME"]), cname);
