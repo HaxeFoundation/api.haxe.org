7ba7b119cde40331cdea2e972d12fe3375a8a538
Aleksandr Kuzmenko <alex@stablex.ru>
2021-02-12 18:30:28 +0300

    allow failures on branches (except development)

--- Gen.hx
+++ Gen.hx
@@ -19,6 +19,7 @@ class Gen {
     inline static var htmlDir = "html";
     inline static var xmlDir = "xml";
     inline static var xmlTagsDir = "xml/refs/tags";
+    inline static var developmentBranch = "development";
     static var skipXmlDirs = ["xml/refs"];
     inline static var themeDir = "theme";
 
@@ -91,14 +92,15 @@ class Gen {
                 var path = Path.join([dir, item]);
                 if (skipXmlDirs.indexOf(path) >= 0 || !isDirectory(path))
                     continue;
-                trace(path);
                 var version = item;
                 var version_long = version;
                 var versionDir, gitRef;
+                var allowFailure = false;
                 switch(versionInfo.find(function(v) return v.name == version)) {
                     case null: // it is not a release, but a branch
                         gitRef = version;
                         versionDir = versionedPath(gitRef);
+                        allowFailure = version != developmentBranch;
                         try {
                             var info:DocInfo = Json.parse(getContent(Path.join([path, "info.json"])));
                             version_long = '${version} @ ${info.commit.substr(0, 7)}';
@@ -127,7 +129,11 @@ class Gen {
                         "-D", "origin", Path.join([origin, versionDir]),
                     ]);
                 }
-                dox(args);
+                try {
+                    dox(args);
+                } catch(e) {
+                    if(!allowFailure) throw e;
+                }
 
                 if (version == latestVersion) {
                     var args = args.concat([
